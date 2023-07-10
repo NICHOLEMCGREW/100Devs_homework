b0fd2b236200136dce19871ae3f266979b7e315e
Nichole <nicholemcgrew@gmail.com>
2023-07-10 13:17:46 -0500

    clock JS30

--- Classes 16-18/main.js
+++ Classes 16-18/main.js
@@ -103,3 +103,15 @@ function sayHello() {
 sayHello()
 
 Number squaring
+
+function square1(x) {
+
+}
+
+console.log(square1(0)); // Must show 0
+console.log(square1(2)); // Must show 4
+console.log(square1(5)); // Must show 25
+
+console.log(square2(0)); // Must show 0
+console.log(square2(2)); // Must show 4
+console.log(square2(5)); // Must show 25
