80813422dbc41bad9e3c1be39da9bd7e24e65cde
Nichole <nicholemcgrew@gmail.com>
2023-07-11 10:35:18 -0500

    finished thejsway 06

---
+++ class-19/main.js
@@ -0,0 +1,181 @@
+const pen = {
+    type: 'ballpoint',
+    color: 'blue',
+    brand: 'Bic'
+};
+pen.color = 'red';
+pen.price = '$2.5';
+
+// console.log(pen.type)
+// console.log(pen.price)
+// console.log(pen.color)
+
+
+const aurora = {
+    name: 'Aurora',
+    health: 150,
+    strength: 25
+};
+
+// console.log(`${aurora.name} has ${aurora.health} health points and ${aurora.strength} as strength`);
+
+aurora.health -= 20;
+aurora.strength += 10;
+
+// console.log(`${aurora.name} has ${aurora.health} health points and ${aurora.strength} as strength`);
+
+function describe(character) {
+    return `${character.name} has ${character.health} health points and ${character.strength} as strength`;
+}
+
+// console.log(describe(aurora))
+
+const aurora2 = {
+    name: "Aurora",
+    health: 150,
+    strength: 25,
+  
+    // Return the character description
+    describe() {
+      return `${this.name} has ${this.health} health points and ${this
+        .strength} as strength`;
+    }
+  };
+  
+//   console.log(aurora2.describe());
+
+// Adding character experience
+
+const aurora3 = {
+    name: 'Aurora',
+    health: 150,
+    strength: 25,
+
+    describe() {
+        return `${this.name} has ${this.health} health points and ${this
+          .strength} as strength and ${this.xp} XP points.`;
+      }
+};
+
+aurora3.xp = 0;
+
+aurora3.health -= 20;
+aurora.strength += 10;
+aurora3.xp += 15;
+// console.log(aurora3.describe());
+
+// Modeling a dog
+
+const dog = {
+    name: 'Fang',
+    species: 'boarhound',
+    size: 75,
+
+    bark() {
+        return 'Grrr! Grrr!'
+    }
+};
+
+// console.log(`${dog.name} is a ${dog.species} dog measuring ${dog.size}`);
+// console.log(`Look, a cat! ${dog.name} barks: ${dog.bark()}`);
+
+// Modeling a circle
+// const r = Number(prompt('Enter the circle radious'));
+const circle = {
+    circumference()  {
+        return 2 * Math.PI * r; 
+    },
+    area() {
+        return Math.PI * r^2;
+    }
+}
+
+// console.log(`Its circumference is ${circle.circumference()}`);
+// console.log(`Its area is ${circle.area()}`);
+
+// Modeling a bank account
+const account = {
+    name: 'Alex',
+    balance: 0,
+    credit(amount) {
+       this.balance += amount;
+       return this.balance;
+    },
+    describe() {
+        return `Owner: ${this.name}, balance ${this.balance}`;
+    }
+}
+
+account.credit += 10;
+
+// console.log(account.describe())
+
+const account2 = function (name, balance) {
+    this.name = name;
+    this.balance = balance;
+    this.credit = function(amount) {
+        this.balance += amount;
+    }
+}
+
+// Manipulating arrays in JavaScript
+
+const movies = ["The Wolf of Wall Street", "Zootopia", "Babysitting"];
+// console.log(movies[2])
+
+for (let i = 0; i < movies.length; i++) {
+    // console.log(movies[i]);
+}
+
+movies.forEach(movie => {
+    // console.log(movie);
+})
+
+for (const movie of movies) {
+    // console.log(movie)
+}
+
+movies.push('Ghostbusters');
+// console.log(movies[3])
+
+movies.unshift('Pacific Rim');
+// console.log(movies[0])
+
+movies.pop();
+// console.log(movies.length);
+// console.log(movies[0])
+// console.log(movies)
+
+// Coding time!
+// Musketeers
+
+const musketeers = ["Athos", "Porthos", "Aramis"];
+
+for (let i = 0; i <= musketeers.length; i++) {
+    // console.log(musketeers[i])
+}
+
+musketeers.push("D'Artagnan");
+
+musketeers.forEach(musketeer => {
+    // console.log(musketeer)
+})
+
+musketeers.splice(2);
+
+for (const musketeer of musketeers) {
+    // console.log(musketeer)
+}
+
+// Sum of values
+
+const values = [3, 11, 7, 2, 9, 10];
+
+function getSum(accumulator, currentValue) {
+    return accumulator + currentValue;
+}
+
+let sum = values.reduce(getSum);
+console.log(sum);
+
+// Array maximum
